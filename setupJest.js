afaeaf077a9af434531de9d302500ceb65edad7d
Jack Reed <phillipjreed@gmail.com>
2021-04-09 16:11:36 -0600

    Update dependencies and support for React 17

--- setupJest.js
+++ setupJest.js
@@ -1,4 +1,4 @@
 import Enzyme from 'enzyme'; // eslint-disable-line import/no-extraneous-dependencies
-import Adapter from 'enzyme-adapter-react-16'; // eslint-disable-line import/no-extraneous-dependencies
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'; // eslint-disable-line import/no-extraneous-dependencies
 
 Enzyme.configure({ adapter: new Adapter() });
